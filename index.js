eeba823215c1a1c3dd661c20a4c6f890d96947d6
SepoupeBrandwarks <sepoupebrandwarks@gmail.com>
2025-05-25 01:49:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { default: makeWASocket, useMultiFileAuthState } = require("@adiwajshing/baileys");
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState('auth_info');
+  const sock = makeWASocket({
+    auth: state
+  });
+
+  sock.ev.on('creds.update', saveCreds);
+
+  sock.ev.on('messages.upsert', async (m) => {
+    console.log('Mensagem recebida:', m);
+  });
+}
+
+startBot();
 
